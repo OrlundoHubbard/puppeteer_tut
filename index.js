133b01a7b1f3e66c6dc8c3b960d9d01ae0f99ef7
Orlundo Hubbard <weareprojectcode@gmail.com>
2023-05-14 21:32:53 -0500

    try catch fix

--- index.js
+++ index.js
@@ -7,10 +7,27 @@ const puppeteer = require('puppeteer');
         userDataDir: "./tmp"
     });
     const page = await browser.newPage();
-    await page.goto('https://www.amazon.com/b?node=13585437011&pf_rd_r=CAFWTS1RYB1VZR1TN6ME&pf_rd_p=9e08f9e6-3c06-4e1a-9285-f6e9e1b21612&pd_rd_r=2e1bcd57-bb49-4b65-abad-230cc88000ee&pd_rd_w=6y8D4&pd_rd_wg=RGg8v&ref_=pd_gw_unk');
+    await page.goto('https://www.amazon.com/s?rh=n%3A16225007011&fs=true&ref=1p_16225007011_sar');
 
-    const tweetHandles = await page.$$('.Grid-module__gridDisplayGrid_2X7cDTY7pjoTwwvSRQbt9Y');
-    
+    const productHandles = await page.$$('div.s-main-slot.s-result-list.s-search-results.sg-row > .s-result-item');
+
+    // loop through all products
+    for (const producthandle of productHandles) {
+
+        try {
+            // pass the single handle below
+            const title = await page.evaluate((el) => el.querySelector("h2 > a > span").textContent, producthandle)
+
+
+            console.log(title)
+
+        } catch (error) {
+
+        }
+
+
+    }
+
 
     // await browser.close();
 })();
